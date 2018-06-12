a61530cc3ab512fc4c62b62c77d7102fb5d011fb
CJarrell <colbyj@Colbys-iMac.local>
2018-06-11 21:02:24 -0700

    The Gresham Rotary Clubs 64th Annual Steak Fry: First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# steakfront
+
+> A NodeJS website for promoting the Gresham Rotarian Club's 64th Annnual Steak Fry Fundraiser. Learn about the steak fry and fundraiser, buy tickets and volunteer. Developed by Colby Jarrell | Creative Consultant with NPM, NodeJS, VueJS, AuthorizeAPI, and Bulma.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/main.js
@@ -0,0 +1,43 @@
+// Environment Config
+if (process.env.NODE_ENV === 'production') {
+	window.endpoint = 'https://ribeye.herokuapp.com';
+} else {
+	window.endpoint = 'http://localhost:3000';
+}
+
+// Global Variables
+window.moment = require('moment');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './components/App.vue'
+import Home from './components/Home.vue'
+import Tickets from './components/Tickets.vue'
+import Order from './components/Order.vue'
+import Complete from '/.components/Complete.vue'
+import Contact from './components/Contact.vue'
+
+Vue.use(VueRouter);
+
+// Register Routes
+const routes = [
+	{ name: 'home', path: '/', component: Home },
+	{ name: 'tickets', path: '/', component: Tickets },
+	{ name: 'order', path: '/', component: Order },
+	{ name: 'order-complete', path: '/order-complete', component: Complete }
+];
+
+const router = new VueRouter({
+	routes
+});
+
+new Vue({
+	router,
+ 	el: '#app',
+ 	data(){
+ 		return {};
+ 	}
+  	render: h => h(App)
+});
